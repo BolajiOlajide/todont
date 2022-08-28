e72b179b65b97b245cdfc94a7d8f2a15eb69fe86
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-08-28 02:01:13 +0100

    update types

--- internal/types/item.go
+++ internal/types/item.go
@@ -2,15 +2,14 @@ package types
 
 import "fmt"
 
-type Items struct {
+// Item represents an action you don't want to do anymore
+type Item struct {
 	ID        int    `json:"id"`
 	Content   string `json:"content"`
-	Done      bool   `json:"done"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
-	DeletedAt string `json:"deleted_at"`
 }
 
-func (i *Items) String() string {
+func (i *Item) String() string {
 	return fmt.Sprintf("Items<ID=%d, Content=%s>", i.ID, i.Content)
 }
--- internal/types/user.go
+++ internal/types/user.go
@@ -2,15 +2,14 @@ package types
 
 import "fmt"
 
+// User represents an entity that can log in to the Todont app.
 type User struct {
 	ID        int    `json:"id"`
 	Name      string `json:"name"`
 	Password  string `json:"password"`
-	Active    bool   `json:"active"`
 	Email     string `json:"email"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
-	DeletedAt string `json:"deleted_at"`
 }
 
 func (u *User) String() string {
